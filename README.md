a59f6b216dc72539f0258663cfc5344127738752
Blake Rain <blake.rain@blakerain.com>
2023-10-24 12:21:03 +0100

    chore: Initial code commit

--- README.md
+++ README.md
@@ -1,3 +1,185 @@
 # poem-route-macro
 
-Simple macro for easier Poem route definitions
+Provides a simple macro to ease the definition of routes in [Poem].
+
+## Example
+
+Here is an example use of the macro to construct a Poem [`Route`] that has a number of end-points
+and a nested static files endpoint.
+
+```rust
+use poem::{endpoint::StaticFilesEndpoint, EndpointExt, IntoEndpoint, Route};
+
+fn build_routes(my_data: MyData) -> impl IntoEndpoint {
+    define_routes!(Route::new(), {
+        // Nest a static files endpoint
+        *"/static"      { StaticFilesEndpoint::new("./static") }
+
+        // Standard routes
+        "/"             index::index        GET
+        "/pastes"       paste::pastes       GET
+        "/pastes/:id"   paste::paste        GET POST
+
+        // A nested route for administration
+        *"/admin"       { admin::build_routes() }
+    })
+    .data(my_data)
+}
+```
+
+The above will generate the following code:
+
+```rust
+fn build_routes() -> Route {
+    Route::new()
+        .nest("/static", StaticFilesEndpoint::new("./static"))
+        .at("/", poem::get(index::get_index))
+        .at("/pastes", poem::get(paste::get_pastes))
+        .at("/pastes/:id", poem::get(paste::get_paste).post(post_paste))
+        .nest("/admin", admin::build_routes())
+}
+```
+
+## How to use
+
+The optional first argument to the `define_routes` macro is the expression to which all the routes
+are applied in a builder pattern. If this expression is missing, it defaults to `Route::new()`. As
+such, these two are equivalent:
+
+```rust
+fn build_routes_no_expr() -> Route {
+    define_routes!({
+        "/" index GET
+    })
+}
+
+fn build_routes_with_expr() -> Route {
+    define_routes!(Route::new(), {
+        "/" index GET
+    })
+}
+```
+
+This can be useful, as you might want to have something _before_ the endpoints are added
+builder-style to the router.
+
+After the optional expression, the routes are specified, wrapped in braces.
+
+```rust
+define_routes!({
+    // routes go here
+})
+```
+
+There are two routes supported: nested and normal.
+
+### Nested Endpoints
+
+A nested route is indicated by an asterisk, followed by the path on which to nest the endpoint.
+
+```rust
+define_routes!({
+    // A nested route under "/static", e.g. "/static/foo"
+    *"/static" { my_nested_router }
+})
+```
+
+After the path string there should be an expression block, being some Rust statements in braces.
+This code should evaluate to something that implements `IntoEndpoint`.
+
+This can be useful to nest multiple routers together:
+
+```rust
+fn admin_routes() -> Route {
+    define_route!({
+        "/"         admin::index    GET
+        "/users"    admin::users    GET POST
+    })
+}
+
+fn post_routes() -> Route {
+    define_route!({
+        "/"         posts::posts    GET
+        "/:id"      posts::post     GET POST
+    })
+}
+
+fn build_routes() -> Route {
+    define_route!({
+        *"/admin"   { admin_routes() }
+        *"/posts"   { post_routes() }
+    })
+}
+```
+
+### Normal Endpoints
+
+Normal endpoints are specified by the path string, then the handler name template, followed by a
+list of methods. The handler name template is a Rust path, such as `handler` or `module::handler`.
+This path is more like that found in a `use` statement, in that it cannot include generic parameters
+in any of its segments.
+
+The handler name template is modified for each method by prefixing the method, lower-case, to the
+last element in the handler name template, separated by an underscore. For example, if the handler
+name template was `s3::bucket` and the methods where `GET` and `POST`, the handler names generated
+will be `s3::get_bucket` and `s3::post_bucket`
+
+Consider the following set of handlers:
+
+```rust
+// Let's define a handler in our current module that we'll use for the "/" route.
+#[handler]
+fn get_index() -> poem::Result<()> { todo!() }
+
+// Define a couple more handlers for signing in under "/user/signin"
+#[handler]
+fn get_user_signin() -> poem::Result<()> { todo!() }
+#[handler]
+fn post_user_signin() -> poem::Result<()> { todo!() }
+
+// Now let's define a couple of handlers for S3 buckets in their own module.
+mod s3 {
+    #[handler]
+    pub fn get_bucket() -> poem::Result<()> { todo!() }
+
+    #[handler]
+    pub fn post_bucket() -> poem::Result<()> { todo!() }
+}
+```
+
+We can then wire-up these handlers with the `define_route` as follows:
+
+```rust
+fn build_routes() -> Route {
+    define_route!({
+        // Handler template 'index' becomes 'get_index'
+        "/"             index           GET
+
+        // Handler template 'user_signin' becomes 'get_user_signin' and 'post_user_signin'
+        "/user/signin"  user_signin     GET POST
+
+        // Wire up the S3 bucket handlers the same way.
+        "/s3/:bucket"   s3::bucket      GET POST
+    })
+}
+```
+
+## Grammar
+
+The grammar for this simple routing table DSL is given in the following rough eBNF:
+
+```ebnf
+body = { EXPR "," } "{" routes "}" ;
+
+routes = route { route } ;
+
+route = "*" LIT_STR EXPR_BLOCK
+      |     LIT_STR path methods
+      ;
+
+path = IDENT { "::" IDENT } ;
+
+methods = method { method } ;
+
+method = "GET" | "POST" | "DELETE" | "PUT" ;
+```
---
+++ src/lib.rs
@@ -0,0 +1,324 @@
+use proc_macro::TokenStream;
+use quote::{format_ident, quote, IdentFragment, ToTokens, TokenStreamExt};
+use syn::{
+    braced,
+    parse::{Parse, ParseStream},
+    parse_macro_input, Token,
+};
+
+struct NestedRoute {
+    path: syn::LitStr,
+    endpoint: syn::ExprBlock,
+}
+
+impl Parse for NestedRoute {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        input.parse::<Token![*]>()?;
+        let path = input.parse()?;
+        let endpoint = input.parse()?;
+        Ok(Self { path, endpoint })
+    }
+}
+
+impl NestedRoute {
+    fn cleanup_endpoint(&self) -> proc_macro2::TokenStream {
+        let Self { endpoint, .. } = self;
+
+        // This is a cheeky shortcut to avoid warnings from Clippy insisting that we remove the
+        // braces around a method argument. This is because the nested endpoint might be a simple
+        // expression that Clippy, quite rightly, asserts need not be wrapped in braces. To reduce
+        // the warnings from the generated code, if we find a single expression in the ExprBlock,
+        // then we just reduce to that expression.
+        if endpoint.block.stmts.len() == 1 {
+            if let Some(syn::Stmt::Expr(expr, _)) = endpoint.block.stmts.first() {
+                return quote! {
+                    #expr
+                };
+            }
+        }
+
+        quote! {
+            #endpoint
+        }
+    }
+
+    fn render(&self) -> proc_macro2::TokenStream {
+        let Self { path, .. } = self;
+        let endpoint = self.cleanup_endpoint();
+
+        quote! {
+          .nest(#path, #endpoint)
+        }
+    }
+}
+
+mod keyword {
+    syn::custom_keyword!(GET);
+    syn::custom_keyword!(POST);
+    syn::custom_keyword!(PUT);
+    syn::custom_keyword!(DELETE);
+}
+
+enum Method {
+    Get(keyword::GET),
+    Post(keyword::POST),
+    Put(keyword::PUT),
+    Delete(keyword::DELETE),
+}
+
+impl Method {
+    fn render(&self) -> &'static str {
+        match self {
+            Self::Get(_) => "get",
+            Self::Post(_) => "post",
+            Self::Put(_) => "put",
+            Self::Delete(_) => "delete",
+        }
+    }
+}
+
+impl IdentFragment for Method {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        core::fmt::Display::fmt(self.render(), f)
+    }
+
+    fn span(&self) -> Option<proc_macro2::Span> {
+        Some(match self {
+            Self::Get(kw) => kw.span,
+            Self::Post(kw) => kw.span,
+            Self::Put(kw) => kw.span,
+            Self::Delete(kw) => kw.span,
+        })
+    }
+}
+
+impl ToTokens for Method {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let ident = match self {
+            Self::Get(kw) => proc_macro2::Ident::new("get", kw.span),
+            Self::Post(kw) => proc_macro2::Ident::new("post", kw.span),
+            Self::Put(kw) => proc_macro2::Ident::new("put", kw.span),
+            Self::Delete(kw) => proc_macro2::Ident::new("delete", kw.span),
+        };
+
+        tokens.append(ident);
+    }
+}
+
+impl Parse for Method {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(keyword::GET) {
+            Ok(Self::Get(input.parse::<keyword::GET>()?))
+        } else if lookahead.peek(keyword::POST) {
+            Ok(Self::Post(input.parse::<keyword::POST>()?))
+        } else if lookahead.peek(keyword::PUT) {
+            Ok(Self::Put(input.parse::<keyword::PUT>()?))
+        } else if lookahead.peek(keyword::DELETE) {
+            Ok(Self::Delete(input.parse::<keyword::DELETE>()?))
+        } else {
+            Err(lookahead.error())
+        }
+    }
+}
+
+struct StandardRoute {
+    path: syn::LitStr,
+    ident: syn::Path,
+    methods: Vec<Method>,
+}
+
+impl Parse for StandardRoute {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let path = input.parse()?;
+        let ident = input.parse()?;
+        let methods = {
+            let mut methods = Vec::new();
+
+            while !input.is_empty() {
+                let lookahead = input.lookahead1();
+                if !lookahead.peek(syn::Ident) {
+                    break;
+                }
+
+                methods.push(input.parse()?);
+            }
+
+            methods
+        };
+
+        Ok(Self {
+            path,
+            ident,
+            methods,
+        })
+    }
+}
+
+fn apply_method(ident: &syn::Ident, method: &Method) -> syn::Ident {
+    format_ident!("{}_{}", method, ident)
+}
+
+fn apply_method_path(head: bool, path: &syn::Path, method: &Method) -> proc_macro2::TokenStream {
+    let mut path = path.clone();
+    if let Some(last) = path.segments.last_mut() {
+        last.ident = apply_method(&last.ident, method)
+    }
+
+    if head {
+        quote! {
+          poem::#method(#path)
+        }
+    } else {
+        quote! {
+          . #method(#path)
+        }
+    }
+}
+
+impl StandardRoute {
+    fn render(&self) -> proc_macro2::TokenStream {
+        let Self {
+            path,
+            ident,
+            methods,
+        } = self;
+
+        let mut builder = Vec::new();
+        for method in methods {
+            builder.push(apply_method_path(builder.is_empty(), ident, method));
+        }
+
+        quote! {
+          .at(#path, #(#builder)*)
+        }
+    }
+}
+
+enum Route {
+    Nested(NestedRoute),
+    Standard(StandardRoute),
+}
+
+impl Parse for Route {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(Token![*]) {
+            Ok(Self::Nested(input.parse()?))
+        } else {
+            Ok(Self::Standard(input.parse()?))
+        }
+    }
+}
+
+impl Route {
+    fn render(&self) -> proc_macro2::TokenStream {
+        match self {
+            Self::Nested(nested) => nested.render(),
+            Self::Standard(standard) => standard.render(),
+        }
+    }
+}
+
+struct Routes {
+    route: syn::Expr,
+    routes: Vec<Route>,
+}
+
+impl Parse for Routes {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let route = input.parse()?;
+        input.parse::<Token![,]>()?;
+
+        let content;
+        braced!(content in input);
+        let mut routes = Vec::new();
+
+        while !content.is_empty() {
+            routes.push(content.parse()?);
+        }
+
+        Ok(Self { route, routes })
+    }
+}
+
+impl Routes {
+    fn render(&self) -> proc_macro2::TokenStream {
+        let Self { route, routes } = self;
+        let routes = routes.iter().map(Route::render);
+
+        quote! {
+          #route #(#routes)*
+        }
+    }
+}
+
+/// Helpful macro to simplify definition of routes.
+///
+/// The first argument to the macro is the name of the `Route` object to which endpoints are to be
+/// added. Each endpoint is defined as a literal string, followed by an identifier, followed by a
+/// list of one or more methods.
+///
+/// For each method, an identifier is formed from the lower-case method, an underscore, and then
+/// the route identifier. For example, a route identifier of `foo` with methods `GET` and `POST`
+/// will generate identifiers `get_foo` and `post_foo`.
+///
+/// The name of a route can be a qualified identifier, such as "module::foo". Any method-specific
+/// modifications are applied to the last identifier in the path: "module::get_foo".
+///
+/// Routes can also be nested by prefixing the route string with an asterisk. In this case, a block
+/// expression is expected after the path string.
+///
+/// As an example, consider the following:
+///
+/// ```ignore
+/// fn add_routes(route: Route) -> Route {
+///     define_routes!(route {
+///       *"/static" { StaticFilesEndpoint::new("./static") }
+///
+///        "/"       index         GET
+///        "/foo"    module::foo   GET POST
+///        "/bar"    bar           GET POST PUT
+///     });
+///
+///     route
+/// }
+/// ```
+///
+/// This will generate the Rust code equivalent to:
+///
+/// ```ignore
+/// fn add_routes(route: Route) -> Route {
+///     route.nest("/static", StaticFilesEndpoint::new("./static"))
+///          .at("/", get(get_root))
+///          .at("/foo", get(module::get_foo).post(module::post_foo))
+///          .at("/bar", get(get_bar).post(post_bar).put(put_bar))
+/// }
+/// ```
+///
+/// Note that the nested static files endpoint is wrapped in braces. This is to help keep the
+/// grammar simple. If the braces are not really needed, they will be stripped from the generated
+/// code.
+///
+/// The grammar for the route specification is as follows:
+///
+/// ```plain
+/// routes := route { route }
+///
+/// route := nested-route | plain-route
+///
+/// nested-route := "*" LIT_STR EXPR_BLOCK
+///
+/// plain-route := LIT_STR path methods
+///
+/// path := IDENT { "::" IDENT }
+///
+/// methods := method { method }
+///
+/// method := "GET" | "POST" | "PUT" | "DELETE"
+/// ```
+///
+#[proc_macro]
+pub fn define_routes(input: TokenStream) -> TokenStream {
+    (parse_macro_input!(input as Routes)).render().into()
+}
